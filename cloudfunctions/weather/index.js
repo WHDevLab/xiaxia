d1a52a51a160cc3a588ef301d43f134975bc0c77
FaiWong <xsd0720@163.com>
2020-09-14 21:47:55 +0800

    111

--- cloudfunctions/weather/index.js
+++ cloudfunctions/weather/index.js
@@ -6,15 +6,10 @@ cloud.init()
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-  console.log(event.location)
   return new Promise((resolve, reject) => {
-    let url = 'https://api.seniverse.com/v3/weather/now.json?key=SIfiejU1AJaimUZaZ&location='+event.location+'&language=zh-Hans&unit=c'
+    let url = 'https://api.breaker.club/weather/index?key=SIfiejU1AJaimUZaZ&lon='+event.lon+'&lat='+event.lat
     request.get(url, function (err, resp, body) {
       return resolve(body)
     })
   })
-  return {
-    event,
-    "a":1
-  }
 }
